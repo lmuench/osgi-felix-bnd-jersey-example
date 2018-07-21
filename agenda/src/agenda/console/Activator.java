7e622c5c3b86f16d04046357a0c8638ca5b8fd52
Ludwig Muench <muench.ludwig@gmail.com>
2018-07-21 10:45:13 +0200

    first commit

---
+++ agenda/src/agenda/console/Activator.java
@@ -0,0 +1,34 @@
+package agenda.console;
+
+import java.util.Properties;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.apache.felix.service.command.CommandProcessor;
+import org.osgi.framework.BundleContext;
+
+import agenda.api.Agenda;
+
+public class Activator extends DependencyActivatorBase {
+
+	@Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+		
+		Properties props = new Properties();
+		props.put(CommandProcessor.COMMAND_SCOPE, "agenda");
+		props.put(CommandProcessor.COMMAND_FUNCTION, new String[] {
+			"listConferences", "addConference"
+		});
+
+		manager.add(
+			createComponent()
+			.setInterface(Object.class.getName(), props)
+			.setImplementation(AgendaConsole.class)
+			.add(
+				createServiceDependency()
+				.setService(Agenda.class)
+				.setRequired(true)
+			)
+		);
+	}
+}
---
+++ agenda/src/agenda/service/simple/Activator.java
@@ -0,0 +1,20 @@
+package agenda.service.simple;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+
+import agenda.api.Agenda;
+
+public class Activator extends DependencyActivatorBase {
+
+	@Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+		
+		manager.add(
+			createComponent()
+			.setInterface(Agenda.class.getName(), null)
+			.setImplementation(SimpleAgendaService.class)
+		);
+	}
+}
