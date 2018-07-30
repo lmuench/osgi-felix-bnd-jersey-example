3fca307deb2d5709e900f1d9249f400321d1593a
Ludwig Muench <muench.ludwig@gmail.com>
2018-07-30 21:11:22 +0200

    add GET conference/:name endpoint

--- agenda/src/agenda/rest/AgendaRest.java
+++ agenda/src/agenda/rest/AgendaRest.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import agenda.api.Agenda;
 import agenda.api.Conference;
 
 
-@Path("agenda")
+@Path("/conferences")
 public class AgendaRest {
 	private volatile Agenda agenda;
 	
@@ -31,6 +32,24 @@ public class AgendaRest {
 		}
 		
 		return res;
+	}
+	
+	@GET
+	@Path("{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Conference getConferences(@PathParam("name") String name) {
+		List<Conference> conferences = agenda.listConferences();
+		
+		if (conferences != null) {			
+			for (Conference conference : conferences) {
+				if (conference.getName().equals(name)) {
+					System.out.println(conference.getName());
+					return conference;					
+				}
+			}
+		}
+		System.out.println("Conference not available");
+		return new Conference();
 	}
 	
 	public void addConference(String name, String location) {
